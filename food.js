1df949456169fe55cd2f1d359ab883dcba7fe5cf
RoseeyaT <124545950+RoseeyaT@users.noreply.github.com>
2023-05-31 01:23:36 +0700

    Add files via upload

---
+++ food.js
@@ -0,0 +1,384 @@
+var food = {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "properties": {
+            "province": "กรุงเทพ",
+            "name": "Organic Supply",
+            "open": "10.00 - 20.00 น.",
+            "address": "148 ถ. นาคนิวาส แขวงลาดพร้าว เขตลาดพร้าว กรุงเทพมหานคร 10230",
+            "contact": "0-2070-7996",
+            "link": "https://www.facebook.com/organicsupply.bkk"
+          },
+          "geometry": {
+            "coordinates": [
+              100.60682887606174,
+              13.799831244629004
+            ],
+            "type": "Point"
+          },
+          "id": 0
+        },
+        {
+          "type": "Feature",
+          "properties": {
+            "province": "กรุงเทพ",
+            "name": "Jones' Salad ",
+            "open": "10.00 - 22.00 น.",
+            "address": "เอสพละนาด รัชดา ห้องเลขที่ 2 G18 99ชั้นG ถ. รัชดาภิเษก แขวงดินแดง เขตดินแดง กรุงเทพมหานคร 10500",
+            "contact": "091-198-4188",
+            "link": "https://jonessalad.com/"
+          },
+          "geometry": {
+            "coordinates": [
+              100.56975559124618,
+              13.765881044516021
+            ],
+            "type": "Point"
+          }
+        },
+        {
+          "type": "Feature",
+          "properties": {
+            "province": "กรุงเทพ",
+            "name": "DJ Poom Menu",
+            "open": "09.00 - 19.00 น.",
+            "address": "244 ซอย พิบูลวัฒนา 7 แขวงสามเสนใน เขตพญาไท กรุงเทพมหานคร 10400",
+            "contact": "090-908-9000",
+            "link": "https://lin.ee/9TQ85Ra"
+          },
+          "geometry": {
+            "coordinates": [
+              100.53638591845896,
+              13.784033370047993
+            ],
+            "type": "Point"
+          }
+        },
+        {
+          "type": "Feature",
+          "properties": {
+            "name": "Broccoli Revolution",
+            "province": "กรุงเทพ",
+            "open": "10.00 - 21.00 น.",
+            "address": "899 ถ. สุขุมวิท แขวงคลองตันเหนือ เขตวัฒนา กรุงเทพมหานคร 10110",
+            "contact": "095-251-9799",
+            "link": "http://www.broccolirevolution.com/"
+          },
+          "geometry": {
+            "coordinates": [
+              100.57537845092918,
+              13.726675132526765
+            ],
+            "type": "Point"
+          }
+        },
+        {
+          "type": "Feature",
+          "properties": {
+            "name": "Farmfactory: Silom complex Branch",
+            "open": "10.30 - 21.00 น.",
+            "province": "กรุงเทพ",
+            "address": "อาคารสีลมคอมเพล็กซ์ 2fl (ใกล้ทางเชื่อม BTS ศาลาแดง 191 ถนน สีลม แขวงสีลม เขตบางรัก กรุงเทพมหานคร 10500",
+            "contact": "063-215-1015",
+            "link": "https://www.farmfactoryworld.com/"
+          },
+          "geometry": {
+            "coordinates": [
+              100.53469723250038,
+              13.728610013649472
+            ],
+            "type": "Point"
+          }
+        },
+        {
+          "type": "Feature",
+          "properties": {
+            "name": "Home Fresh Hydrofarm",
+            "open": "10.00–21.00 น.",
+            "address": "2 ซอยรามคำแหง 167 สะพานสูง , กรุงเทพฯ 10240",
+            "contact": "02-917-2445",
+            "link": "www.facebook.com/homefreshrestaurant",
+            "province": "กรุงเทพ"
+          },
+          "geometry": {
+            "coordinates": [
+              100.7019395009259,
+              13.793436293736107
+            ],
+            "type": "Point"
+          }
+        },
+        {
+          "type": "Feature",
+          "properties": {
+            "province": "กรุงเทพ",
+            "name": "สวนผักน้ำ",
+            "open": "09:00 - 14:00 น. (หยุดทุกวันจันทร์)",
+            "address": "17/136 ถนนสุคนธสวัสดิ์ แขวงลาดพร้าว เขตลาดพร้าว กรุงเทพฯ",
+            "contact": "081-816-6741",
+            "link": "https://www.facebook.com/people/%E0%B8%AA%E0%B8%A7%E0%B8%99%E0%B8%9C%E0%B8%B1%E0%B8%81%E0%B8%99%E0%B9%89%E0%B8%B3-%E0%B8%AA%E0%B8%B8%E0%B8%84%E0%B8%99%E0%B8%98%E0%B8%AA%E0%B8%A7%E0%B8%B1%E0%B8%AA%E0%B8%94%E0%B8%B4%E0%B9%8C/100063657185975/"
+          },
+          "geometry": {
+            "coordinates": [
+              100.62833836560316,
+              13.835973348250334
+            ],
+            "type": "Point"
+          }
+        },
+        {
+          "type": "Feature",
+          "properties": {
+            "province": "กรุงเทพ",
+            "name": "Patom Organic Living",
+            "open": "09.30 – 18.00 น.",
+            "address": "9, 2 ซอย พร้อมพรรค แขวง คลองตันเหนือ เขต วัฒนา กรุงเทพฯ",
+            "contact": "02 084 8649",
+            "link": "https://www.patom.com/"
+          },
+          "geometry": {
+            "coordinates": [
+              100.57898968222509,
+              13.738614991834467
+            ],
+            "type": "Point"
+          }
+        },
+        {
+          "type": "Feature",
+          "properties": {
+            "name": "Veganerie Concept",
+            "province": "กรุงเทพ",
+            "open": "10.00 – 22.00 น.",
+            "address": "35/2 ถนน สุขุมวิท 24 หลังสวนเบญจสิริ แขวง คลองตัน เขต คลองเตย กรุงเทพฯ",
+            "contact": "02 258 8489",
+            "link": "https://www.facebook.com/Veganerie.Official"
+          },
+          "geometry": {
+            "coordinates": [
+              100.56672825683097,
+              13.72925796458054
+            ],
+            "type": "Point"
+          }
+        },
+        {
+          "type": "Feature",
+          "properties": {
+            "name": "สวนผักโอ้กะจู๋",
+            "open": "09.00 - 21.00 น.",
+            "address": "ชั้น 3 ศูนย์การค้าสยามสแควร์วัน เขตปทุมวัน กรุงเทพฯ",
+            "contact": "08-1980-4216",
+            "link": "https://www.facebook.com/ohkajhu",
+            "province": "กรุงเทพ"
+          },
+          "geometry": {
+            "coordinates": [
+              100.53237940442807,
+              13.744867502972497
+            ],
+            "type": "Point"
+          }
+        },
+        {
+          "type": "Feature",
+          "properties": {
+            "name": "Farm To Table",
+            "province": "กรุงเทพ",
+            "open": "09.00 - 19.00 น.",
+            "address": "ถ.อัษฎางค์ แขวงบูรพาภิรมย์ เขตพระนคร กรุงเทพฯ",
+            "contact": "0-2115-2625",
+            "link": "https://www.facebook.com/FarmToTableOrganicCafe"
+          },
+          "geometry": {
+            "coordinates": [
+              100.49608659283871,
+              13.743633044111704
+            ],
+            "type": "Point"
+          }
+        },
+        {
+          "type": "Feature",
+          "properties": {
+            "name": "Anotai Vegetarian Restaurant",
+            "open": "10.00 - 21.30 น. (ปิดวันพุธ)",
+            "province": "กรุงเทพ",
+            "address": "976/17 ถนนพระราม 9 แขวงบางกะปิ เขตห้วยขวาง กรุงเทพมหานคร",
+            "contact": "0-2641-5366",
+            "link": "https://www.facebook.com/anotaigo/?ref=page_internal"
+          },
+          "geometry": {
+            "coordinates": [
+              100.57036503683923,
+              13.753520641352182
+            ],
+            "type": "Point"
+          }
+        },
+        {
+          "type": "Feature",
+          "properties": {
+            "province": "กรุงเทพ",
+            "name": "Rasayana Retreat",
+            "open": "09.00 - 18.00 น.",
+            "address": "ซอยพร้อมมิตร สุขุมวิท 39 เขตวัฒนา กรุงเทพมหานคร",
+            "contact": "0-2662-4803",
+            "link": "https://www.facebook.com/RasayanaRetreat/"
+          },
+          "geometry": {
+            "coordinates": [
+              100.57347256395929,
+              13.735099408125677
+            ],
+            "type": "Point"
+          }
+        },
+        {
+          "type": "Feature",
+          "properties": {
+            "province": "กรุงเทพ",
+            "name": "greenmade cafe",
+            "open": "11.00 - 18.00 น.",
+            "address": "89 ถ. รัชดาภิเษก แขวง ดินแดง เขตดินแดง กรุงเทพมหานค",
+            "contact": "09-8832-4590",
+            "link": "https://www.facebook.com/greenmadecafe"
+          },
+          "geometry": {
+            "coordinates": [
+              100.56835987906413,
+              13.764789142298156
+            ],
+            "type": "Point"
+          }
+        },
+        {
+          "type": "Feature",
+          "properties": {
+            "province": "กรุงเทพ",
+            "name": "คุณเชิญ Vegetarian",
+            "open": "10.30 - 18.00 น.",
+            "address": "952 ถ. สุขุมวิท พระโขนง, เขตคลองเตย กรุงเทพมหานคร",
+            "contact": "0-2951-9529",
+            "link": "https://www.facebook.com/KhunChurnVeggie/"
+          },
+          "geometry": {
+            "coordinates": [
+              100.58461894941257,
+              13.71953060090874
+            ],
+            "type": "Point"
+          }
+        },
+        {
+          "type": "Feature",
+          "properties": {
+            "name": "May Veggie Home",
+            "province": "กรุงเทพ",
+            "open": "10.00 - 21.00 น. (ปิดวันจันทร์)",
+            "address": "8 3 ถนนรัชดาภิเษก แขวงคลองเตย เขตคลองเตย กรุงเทพมหานคร",
+            "contact": "0-2118-2967",
+            "link": "https://www.facebook.com/mayveggiehome/"
+          },
+          "geometry": {
+            "coordinates": [
+              100.56084050883118,
+              13.735384458812774
+            ],
+            "type": "Point"
+          }
+        },
+        {
+          "type": "Feature",
+          "properties": {
+            "province": "กรุงเทพ",
+            "name": "มีดี (MEDE Cuisine)",
+            "open": "07.00 - 20.00 น.",
+            "address": "ชั้น 9 อาคาร Lifestyle Building โรงพยาบาลเทพธารินทร์ ถนนพระรามสี่ แขวงพระโขนง เขตคลองเตย กรุงเทพมหานคร",
+            "contact": "0-2348-7111",
+            "link": "https://www.facebook.com/medecuisine/"
+          },
+          "geometry": {
+            "coordinates": [
+              100.5768984476594,
+              13.714021428530415
+            ],
+            "type": "Point"
+          }
+        },
+        {
+          "type": "Feature",
+          "properties": {
+            "name": "เหลือใจ",
+            "province": "กรุงเทพ",
+            "open": "10:00 - 20:30 น.",
+            "address": "198 ซอยจุฬา 9, แขวงวังใหม่ เขตปทุมวัน กรุงเทพมหานคร 10330",
+            "contact": "080-657-6212",
+            "link": "https://www.facebook.com/luerjai/?locale=th_TH"
+          },
+          "geometry": {
+            "coordinates": [
+              100.52636801507134,
+              13.735326660183716
+            ],
+            "type": "Point"
+          }
+        },
+        {
+          "type": "Feature",
+          "properties": {
+            "province": "กรุงเทพ",
+            "name": "Plantiful",
+            "open": "08.00 – 20.00 น.",
+            "address": "27 ซอยสุขุมวิท 61 แขวงคลองตันเหนือ เขตวัฒนา กรุงเทพฯ 10110",
+            "contact": "091-715-5247",
+            "link": "https://www.plantifulbkk.com/"
+          },
+          "geometry": {
+            "coordinates": [
+              100.58336951958421,
+              13.725247166320116
+            ],
+            "type": "Point"
+          }
+        },
+        {
+          "type": "Feature",
+          "properties": {
+            "name": "Vistro-Vegan Cafe & Restaurant",
+            "province": "กรุงเทพ",
+            "open": "10.30 – 20.30 น.",
+            "address": "46/1 สุขุมวิท 24 แขวงคลองตัน เขตคลองเตย กรุงเทพมหานคร 10110",
+            "contact": "065-998-2201",
+            "link": "https://vistro.foodie-delivery.com/#/home"
+          },
+          "geometry": {
+            "coordinates": [
+              100.56803593171674,
+              13.72721077830333
+            ],
+            "type": "Point"
+          }
+        },
+        {
+          "type": "Feature",
+          "properties": {
+            "name": "VIBE Coffee & Plant-Based Food",
+            "province": "กรุงเทพ",
+            "open": "07.00 – 19.00 น. ",
+            "address": "118 สุขุมวิท 36 ซอยแสนสบาย พระโขนง กรุงเทพมหานคร 10110",
+            "contact": "097-298-5570",
+            "link": "https://www.facebook.com/vibecoffeebkk"
+          },
+          "geometry": {
+            "coordinates": [
+              100.57628514170045,
+              13.7172570273233
+            ],
+            "type": "Point"
+          }
+        }
+      ]
+    }
